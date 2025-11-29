d4351e82cda6149da7886e8f7606bd388c3aafdc
Leonard Excoffier <48970393+excoffierleonard@users.noreply.github.com>
2025-11-29 06:40:14 -0500

    Refactor video compression function to use temporary files and return compressed data

--- src/video.rs
+++ src/video.rs
@@ -1,39 +1,44 @@
 use std::{
-    path::Path,
+    fs,
     process::{Command, Stdio},
 };
 
+use tempfile::NamedTempFile;
+
 use crate::error::Error;
 
-pub fn compress_video(input: &Path, output: &Path) -> Result<(), Error> {
+pub fn compress_video(input_bytes: &[u8]) -> Result<Vec<u8>, Error> {
+    let input_file = NamedTempFile::new()?;
+    let output_file = NamedTempFile::new()?;
+
+    fs::write(input_file.path(), input_bytes)?;
+
     Command::new("ffmpeg")
         .arg("-y")
         .arg("-i")
-        .arg(input)
+        .arg(input_file.path())
         .arg("-c:v")
-        .arg("hevc_nvenc")
-        .arg(output)
+        .arg("hevc_nvenc") // Migration to AV1 when we will have a 40+ GPU
+        .arg(output_file.path())
         .stdout(Stdio::null())
         .stderr(Stdio::null())
         .status()?;
 
-    Ok(())
+    let output_bytes = fs::read(output_file.path())?;
+    Ok(output_bytes)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::fs;
 
     #[test]
     fn compressing_video_does_work() {
-        let input = Path::new("tests/inputs/water-uhd_3840_2160_25fps.mp4");
-        let output = Path::new("tests/outputs/output.mp4");
-        compress_video(input, output).unwrap();
+        let input_bytes = fs::read("tests/inputs/water-uhd_3840_2160_25fps.mp4").unwrap();
+        let compressed_data = compress_video(&input_bytes).unwrap();
+        fs::write("tests/outputs/output.mp4", &compressed_data).unwrap();
 
-        assert!(output.exists(), "Output file was not created");
-        assert!(
-            output.metadata().unwrap().len() > 0,
-            "Output file is empty or corrupted"
-        );
+        assert!(!compressed_data.is_empty(), "Output is empty or corrupted");
     }
 }
